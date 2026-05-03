5b9e2d31aa42d05322c273925ac19ae8d3ce4619
jahid <jahidhasanbsl46@gmail.com>
2026-05-03 19:49:06 +0600

    proxy update

--- src/proxy.js
+++ src/proxy.js
@@ -3,21 +3,27 @@ import { NextResponse } from "next/server";
 import { auth } from "./lib/auth";
 
 export async function proxy(request) {
-const session = await auth.api.getSession({
+  const session = await auth.api.getSession({
     headers: await headers(),
   });
 
-  if (session) {
-    return NextResponse.next();
+  const { pathname } = request.nextUrl;
+
+  
+  if (!session && pathname.startsWith('/all-animals/')) {
+    const loginUrl = new URL("/signin", request.url);
+    return NextResponse.redirect(loginUrl);
   }
 
  
-  const loginUrl = new URL("/signin", request.url);
-  return NextResponse.redirect(loginUrl);
+  if (!session && pathname === '/my_profile') {
+    return NextResponse.redirect(new URL("/signin", request.url));
+  }
+
+  return NextResponse.next();
 }
 
-
-
 export const config = {
-  matcher: ["/my_profile", "/all-animals/:path+"],
+
+  matcher: ["/my_profile", "/all-animals/:path+"], 
 };
