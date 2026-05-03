ea4bd8ab859b14384db69b4c3ca655661a508cbf
jahid <jahidhasanbsl46@gmail.com>
2026-05-03 15:26:39 +0600

    auth update

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -4,27 +4,33 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
 const client = new MongoClient(process.env.MONGO_URI);
 
-const db = client.db("qurbanihut"); 
+const db = client.db("qurbanihut");
 
 export const auth = betterAuth({
-  database: mongodbAdapter(db), 
-  
- 
+  database: mongodbAdapter(db),
+
   baseURL: process.env.BETTER_AUTH_URL,
-  
+
   trustedOrigins: [
     "https://a8-b13-qurbanihat-livestock.vercel.app",
-    "http://localhost:3000"
+    "http://localhost:3000",
   ],
 
   emailAndPassword: {
     enabled: true,
+
+    autoSignIn: true,
   },
-  
+
   socialProviders: {
     google: {
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
     },
+  },
+
+  session: {
+    expiresIn: 60 * 60 * 24 * 7,
+    updateAge: 60 * 60 * 24,
   },
 });
