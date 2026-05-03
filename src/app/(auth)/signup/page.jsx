9c2f182f6ad9cf3c58717f4c6799830e475d6d23
jahid <jahidhasanbsl46@gmail.com>
2026-05-03 23:34:08 +0600

    issu update

--- src/app/(auth)/signup/page.jsx
+++ src/app/(auth)/signup/page.jsx
@@ -15,7 +15,8 @@ import {
 import Link from "next/link";
 import { authClient } from "@/lib/auth-client";
 import { toast, Bounce, ToastContainer } from "react-toastify";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
+
 
 export default function SignUpPage() {
   const router = useRouter();
